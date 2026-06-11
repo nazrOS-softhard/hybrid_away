bf92f7081637f359e03606aaab92392b1e6e7082
назрОС <102623319+nazrOS-softhard@users.noreply.github.com>
2026-06-11 22:29:53 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
 import { Sidebar } from '@/components/layout/Sidebar'
-import { MapPanel } from '@/components/map/MapPanel'
-import { RoutePanel } from '@/components/route/RoutePanel'
-import { RoutesBar } from '@/components/route/RoutesBar'
-import { TravelCards } from '@/components/travel/TravelCards'
-import { SearchBar } from '@/components/search/SearchBar'
+import { AppContent } from '@/components/AppContent'
 
 export default function HomePage() {
   return (
     <div className="flex h-screen w-screen overflow-hidden bg-[#0D0F14]">
       <Sidebar />
-      <main className="flex flex-col flex-1 overflow-hidden">
-        <SearchBar />
-        <div className="flex flex-1 overflow-hidden">
-          <div className="flex flex-col flex-1 overflow-hidden">
-            <MapPanel />
-            <RoutesBar />
-          </div>
-          <RoutePanel />
-        </div>
-        <TravelCards />
-      </main>
+      <AppContent />
     </div>
   )
 }
